36512f62bd6b495cfc56468bef29b11886d644e8
kotomoto <jkotor@gmail.com>
2024-05-07 14:31:42 +0200

    Add location handling

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -4,10 +4,22 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  PlaceLocation({required this.latitude, required this.longitude});
+
+  final double latitude;
+  final double longitude;
+}
+
 class Place {
-  Place({required this.title, required this.image}) : id = uuid.v4();
+  Place({
+    required this.title,
+    required this.image,
+    // required this.location, // todo to be added
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
   final File image;
+  // final PlaceLocation location; // todo to be added
 }
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,111 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() {
+    return _LocationInputState();
+  }
+}
+
+class _LocationInputState extends State<LocationInput> {
+  PlaceLocation? _pickedLocation;
+  var _isGettingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    setState(() {
+      _isGettingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final long = locationData.longitude;
+
+    if (lat == null || long == null) {
+      return;
+    }
+
+    setState(() {
+      _pickedLocation = PlaceLocation(latitude: lat, longitude: long);
+      _isGettingLocation = false;
+    });
+
+    print('Lat: ${locationData.latitude}');
+    print('Long: ${locationData.longitude}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewContent = Text(
+      'No location chosen',
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+    );
+
+    if (_pickedLocation != null) {
+      previewContent = Text('Got location');
+    }
+
+    if (_isGettingLocation) {
+      previewContent = const CircularProgressIndicator();
+    }
+
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+                width: 1,
+                color: Theme.of(context).colorScheme.primary.withOpacity(0.2)),
+          ),
+          alignment: Alignment.center,
+          height: 170,
+          width: double.infinity,
+          child: previewContent,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton.icon(
+              icon: const Icon(Icons.location_on),
+              label: const Text('Get current location'),
+              onPressed: _getCurrentLocation,
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.map),
+              label: const Text('Select on map'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
